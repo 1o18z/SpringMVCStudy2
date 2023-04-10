d0d277deff43d6aae67dfb134d114dccd2711fb5
eunji <qkrdmswl1018@naver.com>
2023-04-10 09:17:50 +0900

    타임리프 기본 기능

---
+++ SpringMVC2/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -0,0 +1,145 @@
+package hello.thymeleaf.basic;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/basic")
+public class BasicController {
+
+    @GetMapping("/text-basic")
+    public String textBasic(Model model) {
+        model.addAttribute("data", "hello");
+        return "basic/text-basic";
+    }
+
+    @GetMapping("/text-unescape")
+    public String textUnescaped(Model model) {
+        model.addAttribute("data", "Hello <b>Spring!</b>");
+        return "basic/text-unescaped";
+    }
+
+    @GetMapping("/variable")
+    public String variable(Model model) {
+        User userA = new User("userA", 10);
+        User userB = new User("userB", 20);
+
+
+        List<User> list = new ArrayList<>();
+
+        list.add(userA);
+        list.add(userB);
+
+        Map<String, User> map = new HashMap<>();
+        map.put("userA", userA);
+        map.put("userB", userB);
+
+        model.addAttribute("user", userA);
+        model.addAttribute("users", list);
+        model.addAttribute("userMap", map);
+        return "basic/variable";
+    }
+
+    @GetMapping("/basic-objects")
+    public String basicObjects(HttpSession session) {
+        session.setAttribute("sessionData", "Hello Session");
+        return "basic/basic-objects";
+    }
+
+
+    @Component("helloBean")
+    static class HelloBean {
+        public String hello(String data) {
+            return "Hello" + data;
+        }
+    }
+
+    @GetMapping("/date")
+    public String date(Model model) {
+        model.addAttribute("localDate", LocalDateTime.now());
+        return "basic/dateTime";
+
+    }
+
+    @GetMapping("/link")
+    public String link(Model model) {
+        model.addAttribute("param1", "data1");
+        model.addAttribute("param2", "data2");
+        return "basic/link";
+    }
+
+    @GetMapping("/literal")
+    public String literal(Model model) {
+        model.addAttribute("data", "Spring!");
+        return "/basic/literal";
+    }
+
+    @GetMapping("/operation")
+    public String operation(Model model) {
+        model.addAttribute("nullData", null);
+        model.addAttribute("data", "Spring!");
+        return "/basic/operation";
+    }
+
+    @GetMapping("/attribute")
+    public String attribute(Model model) {
+        return "/basic/attribute";
+    }
+
+    @GetMapping("/each")
+    public String each(Model model) {
+        addUser(model);
+        return "/basic/each";
+    }
+
+    @GetMapping("/condition")
+    public String condition(Model model) {
+        addUser(model);
+        return "/basic/condition";
+    }
+
+    @GetMapping("/comments")
+    public String comments(Model model) {
+        model.addAttribute("data", "Spring!");
+        return "/basic/comments";
+    }
+
+    @GetMapping("/block")
+    public String block(Model model) {
+        addUser(model);
+        return "/basic/block";
+    }
+
+    private void addUser(Model model) {
+
+        List<User> list = new ArrayList<>();
+
+        list.add(new User("userA", 10));
+        list.add(new User("userB", 20));
+        list.add(new User("userC", 30));
+        model.addAttribute("users", list);
+    }
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    static class User {
+        private String username;
+        private int age;
+
+    }
+}
