6533cee3cd2ced0cd43a1f8ff8017ca83c578bda
eunji <qkrdmswl1018@naver.com>
2023-04-11 14:21:45 +0900

    오류 코드와 메시지 처리3

--- validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -171,6 +171,7 @@ public class ValidationItemControllerV2 {
         // 검증 로직
         if (!StringUtils.hasText(item.getItemName())) { // 상품명칸에 글자가 없다면
             bindingResult.rejectValue("itemName", "required");
+//            new String[]{"required.item.itemName", "required"};
         }
         if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
             bindingResult.rejectValue("price", "range", new Object[]{1000, 1000000}, null);
